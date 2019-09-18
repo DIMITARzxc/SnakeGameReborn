2edc6e7787f4286cdfb51bf7497a0a410f6532a6
dimitar.nedilkovski@gmail.com <dimitar.nedilkovski@gmail.com>
2019-09-18 11:20:06 +0300

    Add canvas and fields

---
+++ SnakeGameReborn/Canvas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NConsoleGraphics;
+namespace SnakeGameReborn
+{
+    class Canvas
+    {
+        private int _width;
+        private int _height;
+        private uint _color;
+        public Canvas(int clientWidth,int clientHeight, uint color)
+        {
+            _width = clientHeight;
+            _height = clientHeight;
+            _color = color;
+        }
+        public void Draw(ConsoleGraphics graphics)
+        {
+            graphics.FillRectangle(_color, 0, 0, _width, _height);
+        }
+
+    }
+}
